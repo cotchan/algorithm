3ee29910dfbf622dc770e22f78a066cbe36bea41
Chanyoung <zhtcks@gmail.com>
2021-04-28 08:33:56 +0900

    Update PG60057.java

--- java/PROGRAMMERS/PG60057.java
+++ java/PROGRAMMERS/PG60057.java
@@ -1,5 +1,3 @@
-package PROGRAMMERS;
-
 import java.util.*;
 
 class Solution {
@@ -28,15 +26,15 @@ class Solution {
         			//rest
         			if (idx != 0)
         			{
-            		    int beforeCnt = stateMap.get(before);
+            			int beforeCnt = stateMap.get(before);
             			
-            	        if (beforeCnt != 1)
-            		    {
-            			    sb.append(beforeCnt);
-            		    }
+            			if (beforeCnt != 1)
+            			{
+            				sb.append(beforeCnt);
+            			}
             			
-            		    sb.append(before);
-            		    stateMap.remove(before);	
+            			sb.append(before);
+            			stateMap.remove(before);	
         			}
         			
         			stateMap.put(target, 1);
