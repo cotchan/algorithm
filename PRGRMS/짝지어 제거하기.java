9bb66d8e5282983fb0f4ee069bb2a678d787cb52
cotchan <zhtcks@gmail.com>
2022-03-18 18:23:36 +0900

    [PRGRMS]12973번: 짝지어 제거하기

---
+++ PRGRMS/짝지어 제거하기.java
@@ -0,0 +1,28 @@
+package prgrms;
+
+
+import java.util.Stack;
+
+class Solution {
+
+    public int solution(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); ++i) {
+            char alphabet = s.charAt(i);
+
+            if (stack.isEmpty() || (stack.peek() != alphabet)) {
+                stack.add(alphabet);
+            } else if (stack.peek() == alphabet) {
+                stack.pop();
+            }
+        }
+
+        if (stack.isEmpty()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
