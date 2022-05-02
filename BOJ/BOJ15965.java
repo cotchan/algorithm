0abab68f4f0d30af7293eff9691cacc35782dde6
cotchan <zhtcks@gmail.com>
2022-05-02 22:33:00 +0900

    [BOJ]15965.java

---
+++ BOJ/BOJ15965.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+    public static int K;
+    public static boolean[] isPrime;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        K = Integer.parseInt(br.readLine());
+        final int MAX_VALUE = 100 * K;
+
+        isPrime = new boolean[MAX_VALUE+1];
+        Arrays.fill(isPrime, true);
+
+        for (int i = 2; i <= (int) Math.sqrt(MAX_VALUE); ++i) {
+            if (isPrime[i]) {
+                for (int j = i + i; j < MAX_VALUE; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        if (K == 1) {
+            System.out.println(2);
+            return;
+        }
+
+        int primeCnt = 2;
+        int value = 3;
+
+        while (true) {
+            if (primeCnt == K) {
+                break;
+            }
+
+            value += 2;
+
+            if (isPrime[value]) {
+                primeCnt++;
+            }
+        }
+
+        System.out.println(value);
+    }
+}
