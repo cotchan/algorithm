6a3b77cbb844cc61a131d6788d316089d029effc
cotchan <zhtcks@gmail.com>
2024-05-06 17:53:31 +0900

    [BOJ]7576번: 토마토

---
+++ python/boj/BOJ7576_deque.py
@@ -0,0 +1,65 @@
+from collections import deque
+
+# n, m
+m, n = map(int, input().split())
+
+board = [[0 for _ in range(m)] for _ in range(n)]
+visited = [[False for _ in range(m)] for _ in range(n)]
+
+dy = [-1, 1, 0, 0]
+dx = [0, 0, -1, 1]
+
+for i in range(n):
+    arr = list(map(int, input().split()))
+    board[i] = arr
+
+
+def safe(board, y, x):
+    ySize = len(board)
+    xSize = len(board[0])
+    return 0 <= y and y < ySize and 0 <= x and x < xSize
+
+
+def bfs(board, n, m):
+    result = 0
+    tomatoCnt = 0
+    searchCnt = 0
+    que = deque()
+    for y in range(n):
+        for x in range(m):
+            if board[y][x] == 1:
+                que.append((y, x, 0))
+                visited[y][x] = True
+            if board[y][x] != -1:
+                tomatoCnt += 1
+
+    while len(que) > 0:
+        loopSize = len(que)
+
+        for _ in range(loopSize):
+            searchCnt += 1
+            y, x, cnt = que.popleft()
+
+            result = max(result, cnt)
+
+            for dir in range(4):
+                ny = y + dy[dir]
+                nx = x + dx[dir]
+
+                if not safe(board, ny, nx):
+                    continue
+                if visited[ny][nx] == True:
+                    continue
+                if board[ny][nx] != 0:
+                    continue
+
+                visited[ny][nx] = True
+                que.append((ny, nx, cnt+1))
+
+    if searchCnt == tomatoCnt:
+        return result
+    else:
+        return -1
+
+
+print(bfs(board, n, m))
